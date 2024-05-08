9cdd83463a1f6b3881205f416de59c552c5b5de7
Aaron <cherhanglim0227@gmail.com>
2024-05-08 08:35:06 +0800

    Upload code batch 4

---
+++ excelReseearch/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 ghg_report.py 65a8ad11e578c4b6b6fffc4e 2023 production tw
+python3 ghg_report.py 65a8ad11e578c4b6b6fffc4e 2023 production en
+python3 ghg_report.py 65a8ad11e578c4b6b6fffc4e 2023 production de
+python3 ghg_report.py 65a8ad11e578c4b6b6fffc4e 2023 production jp
+python3 ghg_report.py 65a8ad11e578c4b6b6fffc4e 2023 production it
+python3 ghg_report.py 65a8ad11e578c4b6b6fffc4e 2023 production th
+python3 ghg_report.py 65a8ad11e578c4b6b6fffc4e 2023 production zh
